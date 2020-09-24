66e9230a87e97d2844eb0565f29be3d7b2223f72
Saksham Puri <sakshampuri1999@gmail.com>
2020-09-24 16:22:26 +0530

    Added common front end

---
+++ ECSE352L - Crypto/README.md
@@ -0,0 +1,11 @@
+## Cryptography Course Labs
+
+The public folder has been created to contain a common front end.
+
+Features of the client is as follows:
+
+- Using websockets for the most part for a session based communication
+- Different cryptography algorithms and techiniques are used in communication
+- Specifics regarding the communication for that particular lab can be found inside the lab info file (.docx or .pdf)
+
+[Saksham Puri](https://sakshampuri.com)
---
+++ ECSE352L - Crypto/public/main.js
@@ -0,0 +1,47 @@
+var ws = new WebSocket('ws://127.0.0.1:5678/');
+
+//defining bootstrap styling classes
+let messageContainer = 'list-group-item';
+let senderName = 'badge';
+
+const getMessageElement = (msg, sender) => {
+	//creating teh current message element
+	let message = document.createElement('li');
+	//styling
+	message.classList.add(messageContainer);
+
+	//adding the tag for sender
+	let tag = document.createElement('span');
+	//styling
+	tag.classList.add(senderName);
+	tag.classList.add('badge-primary');
+	tag.appendChild(document.createTextNode(sender));
+
+	//adding message content
+	content = document.createTextNode(msg);
+	message.appendChild(tag);
+	message.appendChild(document.createElement('br'));
+	message.appendChild(content);
+
+	return message;
+};
+
+const addMessageToChatbox = (element) => {
+	var chatbox = document.getElementsByTagName('ul')[0];
+	chatbox.appendChild(element);
+
+	container = document.getElementById('chatbox');
+	container.scroll(0, 999999999);
+};
+
+ws.onmessage = (event) => {
+	data = event.data;
+	addMessageToChatbox(getMessageElement(data, 'Server'));
+};
+
+const sendMessage = () => {
+	let message = document.getElementById('message').value;
+	addMessageToChatbox(getMessageElement(message, 'Client'));
+};
+
+document.getElementById('sendMessage').addEventListener('click', sendMessage);
